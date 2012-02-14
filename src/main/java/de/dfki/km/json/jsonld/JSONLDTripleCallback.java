488396cf17b6a4d2552505daa331c5452e809353
Peter Ansell <p_ansell@yahoo.com>
2012-02-14 12:49:52 +1000

    some more javadoc on JSONLDTripleCallback

--- src/main/java/de/dfki/km/json/jsonld/JSONLDTripleCallback.java
+++ src/main/java/de/dfki/km/json/jsonld/JSONLDTripleCallback.java
@@ -2,22 +2,25 @@ package de.dfki.km.json.jsonld;
 
 public interface JSONLDTripleCallback {
 	/**
+	 * Construct a triple with three URIs.
 	 * 
 	 * @param s The Subject URI
 	 * @param p The Predicate URI
 	 * @param o The Object URI
-	 * @return The generated triple, or null to force triple generaton to stop
+	 * @return The generated triple, or null to force triple generation to stop
 	 */
 	Object triple(String s, String p, String o);
 	
 	/**
+	 * Constructs a triple with a Literal object, which may or may not contain 
+	 * a language and/or a datatype.
 	 * 
 	 * @param s The Subject URI
 	 * @param p The Predicate URI
 	 * @param value The literal value
 	 * @param datatype The literal datatype
 	 * @param language The literal language (NOTE: may be null if not specified!)
-	 * @return The generated triple, or null to force triple generaton to stop
+	 * @return The generated triple, or null to force triple generation to stop
 	 */
 	Object triple(String s, String p, String value, String datatype, String language);
 }
