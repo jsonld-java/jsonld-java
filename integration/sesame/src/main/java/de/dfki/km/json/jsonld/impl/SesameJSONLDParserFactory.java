403ec366b61f9e8ee5932b1741232d58c7f8c0fb
Peter Ansell <p_ansell@yahoo.com>
2013-03-04 14:45:31 +1000

    Add SesameJSONLDParserFactory

---
+++ integration/sesame/src/main/java/de/dfki/km/json/jsonld/impl/SesameJSONLDParserFactory.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package de.dfki.km.json.jsonld.impl;
+
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.RDFParser;
+import org.openrdf.rio.RDFParserFactory;
+
+/**
+ * An {@link RDFParserFactory} that creates instances of
+ * {@link SesameJSONLDParser}.
+ * 
+ * @author Peter Ansell p_ansell@yahoo.com
+ */
+public class SesameJSONLDParserFactory implements RDFParserFactory {
+
+    @Override
+    public RDFFormat getRDFFormat() {
+	return RDFFormat.JSONLD;
+    }
+
+    @Override
+    public RDFParser getParser() {
+	return new SesameJSONLDParser();
+    }
+
+}
