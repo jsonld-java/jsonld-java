9367d03e67b7f8b911ab3c4d6aa18d77a33a7277
Peter Ansell <p_ansell@yahoo.com>
2013-05-16 16:43:06 +1000

    Ignore RDF-1.0 test that is not compatible with RDF-1.1 literals

--- integration/sesame/src/test/java/com/github/jsonldjava/impl/SesameJSONLDWriterTest.java
+++ integration/sesame/src/test/java/com/github/jsonldjava/impl/SesameJSONLDWriterTest.java
@@ -3,6 +3,12 @@
  */
 package com.github.jsonldjava.impl;
 
+import java.io.IOException;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openrdf.rio.RDFHandlerException;
+import org.openrdf.rio.RDFParseException;
 import org.openrdf.rio.RDFWriterTest;
 
 import com.github.jsonldjava.impl.SesameJSONLDParserFactory;
@@ -17,5 +23,14 @@ public class SesameJSONLDWriterTest extends RDFWriterTest {
     public SesameJSONLDWriterTest() {
 	super(new SesameJSONLDWriterFactory(), new SesameJSONLDParserFactory());
     }
-
+    
+    @Ignore
+    @Test
+    @Override
+	public void testRoundTrip()
+		throws RDFHandlerException, IOException, RDFParseException
+	{
+		// Ignoring test as it is implemented as an RDF-1.0 test that is not compatible 
+		// with RDF-1.1 Typed Literals after translating them to have xsd:String and rdf:langString.
+	}
 }
