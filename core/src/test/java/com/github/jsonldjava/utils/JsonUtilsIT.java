e59154a7c5768dbc39e170332e11d334c5976914
Stian Soiland-Reyes <stian@soiland-reyes.com>
2013-06-13 14:40:34 +0100

    Network-dependent integration tests using failsafe

---
+++ core/src/test/java/com/github/jsonldjava/utils/JsonUtilsIT.java
@@ -0,0 +1,62 @@
+package com.github.jsonldjava.utils;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.net.URL;
+import java.util.Map;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.cache.CacheResponseStatus;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.cache.CachingHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+import org.apache.http.util.EntityUtils;
+import org.junit.Test;
+
+public class JsonUtilsIT {
+
+    @Test
+    public void fromURLredirectHTTPSToHTTP() throws Exception {
+        final URL url = new URL("https://w3id.org/bundle/context");
+        final Object context = JSONUtils.fromURL(url);
+        // Should not fail because of
+        // http://stackoverflow.com/questions/1884230/java-doesnt-follow-redirect-in-urlconnection
+        // http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4620571
+        assertTrue(context instanceof Map);
+        assertFalse(((Map<?, ?>) context).isEmpty());
+    }
+
+    @Test
+    public void fromURLredirect() throws Exception {
+        final URL url = new URL("http://purl.org/wf4ever/ro-bundle/context.json");
+        final Object context = JSONUtils.fromURL(url);
+        assertTrue(context instanceof Map);
+        assertFalse(((Map<?, ?>) context).isEmpty());
+    }
+
+    @Test
+    public void fromURLCache() throws Exception {
+        final URL url = new URL("http://json-ld.org/contexts/person.jsonld");
+        JSONUtils.fromURL(url);
+
+        // Now try to get it again and ensure it is
+        // cached
+        final HttpClient client = new CachingHttpClient(JSONUtils.getHttpClient());
+        final HttpUriRequest get = new HttpGet(url.toURI());
+        get.setHeader("Accept", JSONUtils.ACCEPT_HEADER);
+        final HttpContext localContext = new BasicHttpContext();
+        final HttpResponse respo = client.execute(get, localContext);
+        EntityUtils.consume(respo.getEntity());
+
+        // Check cache status
+        // http://hc.apache.org/httpcomponents-client-ga/tutorial/html/caching.html
+        final CacheResponseStatus responseStatus = (CacheResponseStatus) localContext
+                .getAttribute(CachingHttpClient.CACHE_RESPONSE_STATUS);
+        assertFalse(CacheResponseStatus.CACHE_MISS.equals(responseStatus));
+    }
+
+}
