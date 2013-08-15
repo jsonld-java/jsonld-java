3c8feeb82a64fd30b12d4019a7efe980310ba950
Stian Soiland-Reyes <stian@soiland-reyes.com>
2013-08-15 02:53:28 +0100

    added readURL test

--- integration/jena/src/test/java/com/github/jsonldjava/impl/JenaJSONReaderTest.java
+++ integration/jena/src/test/java/com/github/jsonldjava/impl/JenaJSONReaderTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.hp.hpl.jena.rdf.model.Model;
@@ -18,19 +19,32 @@ public class JenaJSONReaderTest {
     }
 
     @Test
-    public void read() throws Exception {
+    public void readInputStream() throws Exception {
         Model model = ModelFactory.createDefaultModel();
         String jsonld = " { '@id': 'test', \n" +
         		"   'http://example.com/value': 'Test' \n }  ";
         jsonld = jsonld.replace('\'', '"');
         InputStream in = new ByteArrayInputStream(jsonld.getBytes("utf8"));
         
-        String base = "http://example.com/";
-        model.read(in, base, "JSON-LD");
-
+        String uri = "http://example.com/";
+        model.read(in, uri, "JSON-LD");
+//        model.write(System.out, "TURTLE", "");
         assertEquals(1, model.size());
         Resource test = model.getResource("http://example.com/test");
         Property value = model.getProperty("http://example.com/value");
         assertEquals("Test", model.getProperty(test, value).getString());
+    }
+    
+    @Ignore("Integration test")
+    @Test
+    public void readURL() throws Exception {
+        Model model = ModelFactory.createDefaultModel();
+        String url = "http://json-ld.org/test-suite/tests/expand-0002-in.jsonld";
+        model.read(url, "JSON-LD");
+        model.write(System.out, "TURTLE", "");
+//        assertEquals(7, model.size());
+        Resource test = model.getResource("http://example.com/id1");
+        Property value = model.getProperty("http://example.com/term1");
+        assertEquals("v1", model.getProperty(test, value).getString());
     }
 }
