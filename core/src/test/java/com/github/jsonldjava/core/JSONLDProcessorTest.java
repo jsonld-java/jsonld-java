104d5b92cbbbfbb7d0e5d450ae669cb71f14adba
Peter Ansell <p_ansell@yahoo.com>
2013-10-02 13:34:49 +1000

    Use Sesame NQuads parser if possible to compare blank nodes between documents
    Some of the blank nodes are in the predicate position which is disallowed by RDF (excluding the unstandardised Generalised RDF), which makes it still difficult to compare
    
    In addition, need to have a non-standard comparator to ensure that blank nodes in context position are not ignored in equality comparison

--- core/src/test/java/com/github/jsonldjava/core/JSONLDProcessorTest.java
+++ core/src/test/java/com/github/jsonldjava/core/JSONLDProcessorTest.java
@@ -13,17 +13,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.StringReader;
 import java.net.URISyntaxException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -31,6 +35,19 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
+import org.openrdf.model.BNode;
+import org.openrdf.model.Literal;
+import org.openrdf.model.Statement;
+import org.openrdf.model.URI;
+import org.openrdf.model.Value;
+import org.openrdf.model.impl.ValueFactoryImpl;
+import org.openrdf.model.util.ModelUtil;
+import org.openrdf.rio.ParserConfig;
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.RDFParseException;
+import org.openrdf.rio.Rio;
+import org.openrdf.rio.helpers.BasicParserSettings;
+import org.openrdf.rio.helpers.ParseErrorCollector;
 
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -421,8 +438,8 @@ public class JSONLDProcessorTest {
                                 .get("useRdfType");
                     }
                     if (((Map<String, Object>) test.get("option")).containsKey("useNativeTypes")) {
-                        options.useNativeTypes = (Boolean) ((Map<String, Object>) test.get("option"))
-                                .get("useNativeTypes");
+                        options.useNativeTypes = (Boolean) ((Map<String, Object>) test
+                                .get("option")).get("useNativeTypes");
                     }
                 }
                 result = JSONLD.fromRDF(input, options);
@@ -437,9 +454,42 @@ public class JSONLDProcessorTest {
 
         Boolean testpassed = false;
         try {
-            // TODO: for tests that are supposed to fail, a more detailed check
-            // that it failed in the right way is needed
-            testpassed = Obj.equals(expect, result) || failure_expected;
+            if (options.format != null && options.format.equals("application/nquads")) {
+                if (expect instanceof String && result instanceof String) {
+                    ParseErrorCollector expectedErrors = new ParseErrorCollector();
+                    ParseErrorCollector resultErrors = new ParseErrorCollector();
+                    ParserConfig parserConfig = new ParserConfig();
+                    parserConfig.addNonFatalError(BasicParserSettings.VERIFY_DATATYPE_VALUES);
+                    parserConfig.addNonFatalError(BasicParserSettings.VERIFY_LANGUAGE_TAGS);
+                    Set<Statement> expectedModel = new TreeSet<Statement>(
+                            new ContextInsensitiveStatementComparator());
+                    Set<Statement> resultModel = new TreeSet<Statement>(
+                            new ContextInsensitiveStatementComparator());
+                    try {
+                        expectedModel.addAll(Rio.parse(new StringReader((String) expect),
+                                options.base, RDFFormat.NQUADS, parserConfig,
+                                ValueFactoryImpl.getInstance(), expectedErrors));
+                        resultModel.addAll(Rio.parse(new StringReader((String) result),
+                                options.base, RDFFormat.NQUADS, parserConfig,
+                                ValueFactoryImpl.getInstance(), resultErrors));
+                        testpassed = ModelUtil.equals(expectedModel, resultModel);
+                    } catch (RDFParseException e) {
+                        // Sesame cannot handle blank node predicates, so try to
+                        // do a check using our N-Quads parser that can
+                        testpassed = Obj.equals(expect, result) || failure_expected;
+                        if (!testpassed) {
+                            e.printStackTrace();
+                        }
+                    }
+                } else {
+                    testpassed = Obj.equals(expect, result) || failure_expected;
+                }
+            } else {
+                // TODO: for tests that are supposed to fail, a more detailed
+                // check
+                // that it failed in the right way is needed
+                testpassed = Obj.equals(expect, result) || failure_expected;
+            }
             if (testpassed == false) {
                 // System.out.println("failed test!!! details:");
                 // jsonDiff("/", expect, result);
@@ -560,6 +610,134 @@ public class JSONLDProcessorTest {
             if (expect != null && !expect.equals(result)) {
                 System.out.println(parent + " results are not equal: \"" + expect + "\" != \""
                         + result + "\"");
+            }
+        }
+    }
+
+    private static class ContextInsensitiveStatementComparator implements Comparator<Statement> {
+        public final static int BEFORE = -1;
+        public final static int EQUALS = 0;
+        public final static int AFTER = 1;
+
+        @Override
+        public int compare(Statement first, Statement second) {
+            if (first == second) {
+                return EQUALS;
+            }
+
+            if (first.getSubject().equals(second.getSubject())) {
+                if (first.getPredicate().equals(second.getPredicate())) {
+                    if (first.getObject().equals(second.getObject())) {
+                        return EQUALS;
+                    } else {
+                        return new ValueComparator().compare(first.getObject(), second.getObject());
+                    }
+                } else {
+                    return new ValueComparator().compare(first.getPredicate(),
+                            second.getPredicate());
+                }
+            } else {
+                return new ValueComparator().compare(first.getSubject(), second.getSubject());
+            }
+        }
+
+    }
+
+    private static class ValueComparator implements Comparator<Value> {
+        public final static int BEFORE = -1;
+        public final static int EQUALS = 0;
+        public final static int AFTER = 1;
+
+        /**
+         * Sorts in the order nulls>BNodes>URIs>Literals
+         * <p/>
+         * This is due to the fact that nulls are only applicable to contexts,
+         * and according to the OpenRDF documentation, the type of the null
+         * cannot be sufficiently distinguished from any other Value to make an
+         * intelligent comparison to other Values
+         * <p/>
+         * http://www.openrdf.org/doc/sesame2/api/org/openrdf/OpenRDFUtil.html#
+         * verifyContextNotNull(org.openrdf.model.Resource...)
+         * <p/>
+         * BNodes are sorted according to the lexical compare of their
+         * identifiers, which provides a way to sort statements with the same
+         * BNodes in the same positions, near each other
+         * <p/>
+         * BNode sorting is not specified across sessions
+         */
+        @Override
+        public int compare(Value first, Value second) {
+            if (first == null) {
+                if (second == null) {
+                    return EQUALS;
+                } else {
+                    return BEFORE;
+                }
+            } else if (second == null) {
+                // always sort null Values before others, so if the second is
+                // null, but the first wasn't, sort the first after the second
+                return AFTER;
+            }
+
+            if (first == second || first.equals(second)) {
+                return EQUALS;
+            }
+
+            if (first instanceof BNode) {
+                if (second instanceof BNode) {
+                    // if both are BNodes, sort based on the lexical value of
+                    // the internal ID
+                    // Although this sorting is not guaranteed to be consistent
+                    // across sessions,
+                    // it provides a consistent sorting of statements in every
+                    // case
+                    // so that statements with the same BNode are sorted near
+                    // each other
+                    return ((BNode) first).getID().compareTo(((BNode) second).getID());
+                } else {
+                    return BEFORE;
+                }
+            } else if (second instanceof BNode) {
+                // sort BNodes before other things, and first was not a BNode
+                return AFTER;
+            } else if (first instanceof URI) {
+                if (second instanceof URI) {
+                    return ((URI) first).stringValue().compareTo(((URI) second).stringValue());
+                } else {
+                    return BEFORE;
+                }
+            } else if (second instanceof URI) {
+                // sort URIs before Literals
+                return AFTER;
+            }
+            // they must both be Literal's, so sort based on the lexical value
+            // of the Literal
+            else {
+                int cmp = first.stringValue().compareTo(second.stringValue());
+
+                if (EQUALS == cmp) {
+                    URI firstType = ((Literal) first).getDatatype();
+                    URI secondType = ((Literal) second).getDatatype();
+                    if (null == firstType) {
+                        if (null == secondType) {
+                            String firstLang = ((Literal) first).getLanguage();
+                            String secondLang = ((Literal) second).getLanguage();
+
+                            return null == firstLang ? (null == secondLang ? EQUALS : BEFORE)
+                                    : (null == secondLang ? AFTER : firstLang.compareTo(secondLang));
+                        } else {
+                            return BEFORE;
+                        }
+                    } else {
+                        if (null == secondType) {
+                            return AFTER;
+                        } else {
+                            return firstType.stringValue().compareTo(secondType.stringValue());
+                        }
+                    }
+                } else {
+                    return cmp;
+                }
             }
         }
     }
