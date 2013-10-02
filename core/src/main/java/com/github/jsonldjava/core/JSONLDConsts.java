d11ec5492dee38f2971718294beac9318dded1e8
Peter Ansell <p_ansell@yahoo.com>
2013-10-02 11:15:11 +1000

    javadoc JSONLDConsts

--- core/src/main/java/com/github/jsonldjava/core/JSONLDConsts.java
+++ core/src/main/java/com/github/jsonldjava/core/JSONLDConsts.java
@@ -1,6 +1,9 @@
 package com.github.jsonldjava.core;
 
-public class JSONLDConsts {
+/**
+ * URI Constants used in the JSON-LD parser.
+ */
+public final class JSONLDConsts {
 
     public static final String RDF_SYNTAX_NS = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
     public static final String RDF_SCHEMA_NS = "http://www.w3.org/2000/01/rdf-schema#";
