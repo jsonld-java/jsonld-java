fcbba66ea6d171d2cada2319a28301d8de85a650
Peter Ansell <p_ansell@yahoo.com>
2014-01-20 15:01:25 +1100

    Add TestUtils class

---
+++ core/src/test/java/com/github/jsonldjava/utils/TestUtils.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package com.github.jsonldjava.utils;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.apache.commons.io.IOUtils;
+
+/**
+ * @author Peter Ansell p_ansell@yahoo.com
+ * 
+ */
+public class TestUtils {
+
+    public static InputStream copyResourceToFileStream(File testDir, String resource)
+            throws Exception {
+        return new FileInputStream(copyResourceToFile(testDir, resource));
+    }
+
+    public static String copyResourceToFile(File testDir, String resource) throws Exception {
+        String filename = resource.substring(resource.lastIndexOf('/'));
+        String directory = resource.substring(0, resource.lastIndexOf('/'));
+        File nextDirectory = new File(testDir, directory);
+        nextDirectory.mkdirs();
+        File nextFile = new File(nextDirectory, filename);
+        nextFile.createNewFile();
+
+        InputStream inputStream = TestUtils.class.getResourceAsStream(resource);
+        assertNotNull("Missing test resource: " + resource, inputStream);
+
+        IOUtils.copy(inputStream, new FileOutputStream(nextFile));
+        return nextFile.getAbsolutePath();
+    }
+
+    public static String join(Collection<String> list, String delim) {
+        final StringBuilder builder = new StringBuilder();
+        final Iterator<String> iter = list.iterator();
+        while (iter.hasNext()) {
+            builder.append(iter.next());
+            if (!iter.hasNext()) {
+                break;
+            }
+            builder.append(delim);
+        }
+        return builder.toString();
+    }
+
+    private TestUtils() {
+    }
+
+}
