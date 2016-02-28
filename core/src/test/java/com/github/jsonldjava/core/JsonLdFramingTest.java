339c3f76cd8cab33734b0f5aad9713cf0140ce57
Peter Ansell <p_ansell@yahoo.com>
2016-02-28 17:14:38 -0500

    Add positive test for completion in the framing test

--- core/src/test/java/com/github/jsonldjava/core/JsonLdFramingTest.java
+++ core/src/test/java/com/github/jsonldjava/core/JsonLdFramingTest.java
@@ -2,6 +2,8 @@ package com.github.jsonldjava.core;
 
 import com.github.jsonldjava.utils.JsonUtils;
 import java.io.IOException;
+import java.util.Map;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -9,18 +11,14 @@ public class JsonLdFramingTest {
 
     @Test
     public void testFrame0001() throws IOException, JsonLdError {
-        try {
-            Object frame = JsonUtils.fromInputStream(
-                    getClass().getResourceAsStream("/custom/frame-0001-frame.jsonld"));
-            Object in = JsonUtils.fromInputStream(
-                    getClass().getResourceAsStream("/custom/frame-0001-in.jsonld"));
+        Object frame = JsonUtils.fromInputStream(
+                getClass().getResourceAsStream("/custom/frame-0001-frame.jsonld"));
+        Object in = JsonUtils.fromInputStream(
+                getClass().getResourceAsStream("/custom/frame-0001-in.jsonld"));
 
-            JsonLdProcessor.frame(in, frame, new JsonLdOptions());
-        } catch (Throwable t) {
-            t.printStackTrace();
-            
-            fail();
-        }
+        Map<String, Object> frame2 = JsonLdProcessor.frame(in, frame, new JsonLdOptions());
+        
+        assertEquals(2, frame2.size());
     }
 
 }
