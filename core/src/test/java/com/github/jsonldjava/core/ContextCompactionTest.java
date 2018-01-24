28df1a577064723212e62bbbefe81e7d8e575e7c
Peter Ansell <p_ansell@yahoo.com>
2018-01-25 08:37:38 +1100

    WIP context compaction test
    Signed-off-by: Peter Ansell <p_ansell@yahoo.com>

--- core/src/test/java/com/github/jsonldjava/core/ContextCompactionTest.java
+++ core/src/test/java/com/github/jsonldjava/core/ContextCompactionTest.java
@@ -1,14 +1,21 @@
 package com.github.jsonldjava.core;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 import org.junit.Test;
+
+import com.github.jsonldjava.utils.JsonUtils;
 
 public class ContextCompactionTest {
 
@@ -49,4 +56,66 @@ public class ContextCompactionTest {
                 compacted.get("@context") instanceof List);
     }
 
+    @Test
+    public void testCompactValue() throws Exception {
+        final InputStream contexttest = getClass().getResourceAsStream("/custom/compact-0001-in.jsonld");
+        assertNotNull(contexttest);
+        Object jsonld = JsonUtils.fromInputStream(contexttest);
+        System.out.println("Input:");
+        System.out.println(JsonUtils.toPrettyString(jsonld));
+        
+        JsonLdOptions options = new JsonLdOptions();
+        options.setCompactArrays(false);
+        
+        //Map<String, Object> context = new HashMap<>();
+    
+        Object compact = JsonLdProcessor.compact(jsonld, null, options);
+        System.out.println("Output:");
+        System.out.println(JsonUtils.toPrettyString(compact));
+    }
+
+    @Test
+    public void testCompactValueCustom0001() throws Exception {
+        final InputStream input = getClass().getResourceAsStream("/custom/compact-0001-in.jsonld");
+        assertNotNull(input);
+        Object jsonld = JsonUtils.fromInputStream(input);
+        System.out.println("Input:");
+        System.out.println(JsonUtils.toPrettyString(jsonld));
+        
+        JsonLdOptions options = new JsonLdOptions();
+        options.setCompactArrays(true);
+        
+        final InputStream contexttest = getClass().getResourceAsStream("/custom/compact-0001-context.jsonld");
+        assertNotNull(contexttest);
+        Object context = JsonUtils.fromInputStream(contexttest);
+        System.out.println("Context:");
+        System.out.println(JsonUtils.toPrettyString(context));
+    
+        Object compact = JsonLdProcessor.compact(jsonld, context, options);
+        System.out.println("Output:");
+        System.out.println(JsonUtils.toPrettyString(compact));
+    }
+    
+    @Test
+    public void testCompactValue0011() throws Exception {
+        final InputStream input = getClass().getResourceAsStream("/json-ld.org/compact-0011-in.jsonld");
+        assertNotNull(input);
+        Object jsonld = JsonUtils.fromInputStream(input);
+        System.out.println("Input:");
+        System.out.println(JsonUtils.toPrettyString(jsonld));
+        
+        JsonLdOptions options = new JsonLdOptions();
+        options.setCompactArrays(true);
+        
+        final InputStream contexttest = getClass().getResourceAsStream("/json-ld.org/compact-0011-context.jsonld");
+        assertNotNull(contexttest);
+        Object context = JsonUtils.fromInputStream(contexttest);
+        System.out.println("Context:");
+        System.out.println(JsonUtils.toPrettyString(context));
+        //Map<String, Object> context = new HashMap<>();
+    
+        Object compact = JsonLdProcessor.compact(jsonld, context, options);
+        System.out.println("Output:");
+        System.out.println(JsonUtils.toPrettyString(compact));
+    }
 }
