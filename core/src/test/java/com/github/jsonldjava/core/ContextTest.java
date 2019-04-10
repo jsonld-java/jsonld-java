29c195c03cd50d5c41f7f9c4ed3839f296265f4f
Fabian Steeg <steeg@hbz-nrw.de>
2019-04-10 16:54:03 +0200

    Add tests to show workarounds for #248

--- core/src/test/java/com/github/jsonldjava/core/ContextTest.java
+++ core/src/test/java/com/github/jsonldjava/core/ContextTest.java
@@ -1,5 +1,11 @@
 package com.github.jsonldjava.core;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 import org.junit.Test;
 
 import com.google.common.collect.ImmutableMap;
@@ -33,6 +39,36 @@ public class ContextTest {
     @Test(expected = JsonLdError.class)
     public void testIssue141_errorOnEmptyKey_newContext2() {
         new Context(ImmutableMap.of("","http://example.com"), new JsonLdOptions());
+    }
+
+    /* schema.org documentation says some properties can be either Text or URL,
+     * but sets `@type : @id` in the context, e.g. for https://schema.org/roleName:
+     */
+    Map<String,Object> schemaOrg =
+            ImmutableMap.of("roleName", ImmutableMap.of("@id", "http://schema.org/roleName", "@type", "@id"));
+
+    // See https://github.com/jsonld-java/jsonld-java/issues/248
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCompact_uriExpected() throws Exception {
+        JsonLdProcessor.expand(ImmutableMap.of("roleName", "Production Company", "@context", schemaOrg));
+    }
+
+    @Test
+    public void testCompact_forceValue() throws Exception {
+        List<?> value = Arrays.asList(ImmutableMap.of("@value", "Production Company"));
+        Map<String, Object> input = ImmutableMap.of("roleName", value, "@context", schemaOrg);
+        Object output = JsonLdProcessor.expand(input);
+        assertEquals("[{http://schema.org/roleName=[{@value=Production Company}]}]", output.toString());
+    }
+
+    @Test
+    public void testCompact_overrideContext() throws Exception {
+        List<?> context = Arrays.asList(schemaOrg,
+                ImmutableMap.of("roleName", ImmutableMap.of("@id", "http://schema.org/roleName")));
+        Map<String, Object> input = ImmutableMap.of("roleName", "Production Company", "@context", context);
+        Object output = JsonLdProcessor.expand(input);
+        assertEquals("[{http://schema.org/roleName=[{@value=Production Company}]}]", output.toString());
     }
 
 }
