24cf02823be94fd0e1109746d0e4f2f086fe0df9
Fabian Steeg <steeg@hbz-nrw.de>
2019-05-20 10:12:01 +0200

    Add tests for hierarchical and opaque base IRIs
    See https://github.com/jsonld-java/jsonld-java/issues/232

--- core/src/test/java/com/github/jsonldjava/core/ToRDFTest.java
+++ core/src/test/java/com/github/jsonldjava/core/ToRDFTest.java
@@ -4,13 +4,21 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.github.jsonldjava.utils.JsonUtils;
+import com.github.jsonldjava.utils.TestUtils;
 
-public class ArrayContextToRDFTest {
+public class ToRDFTest {
     @Test
     public void toRdfWithNamespace() throws Exception {
 
@@ -42,5 +50,36 @@ public class ArrayContextToRDFTest {
         // Only 'proper' prefixes returned
         assertFalse(rdf.getNamespaces().containsKey("term1"));
 
+    }
+
+    @Test
+    // See https://github.com/jsonld-java/jsonld-java/issues/232
+    public void toRdfWithHttpBaseIri() throws IOException, JsonLdError {
+        testToRdf("/custom/toRdf-0001-in.jsonld", "/custom/toRdf-0001-out.nq", "http://example.org/");
+    }
+
+    @Test
+    // See https://github.com/jsonld-java/jsonld-java/issues/232
+    public void toRdfWithHierarchicalBaseIri() throws IOException, JsonLdError {
+        testToRdf("/custom/toRdf-0001-in.jsonld", "/custom/toRdf-0002-out.nq", "tag:/example/");
+    }
+
+    @Test
+    @Ignore
+    // See https://github.com/jsonld-java/jsonld-java/issues/232#issuecomment-493454096
+    public void toRdfWithOpaqueBaseIri() throws IOException, JsonLdError {
+        testToRdf("/custom/toRdf-0001-in.jsonld", "/custom/toRdf-0003-out.nq", "tag:example/");
+    }
+
+    private void testToRdf(String inFile, String outFile, String baseIri) throws IOException {
+        final Object input = JsonUtils
+                .fromInputStream(getClass().getResourceAsStream(inFile));
+        List<String> resultLines = new BufferedReader(new InputStreamReader(
+                getClass().getResourceAsStream(outFile), StandardCharsets.UTF_8)).lines()
+                        .collect(Collectors.toList());
+        JsonLdOptions options = new JsonLdOptions(baseIri);
+        options.format = JsonLdConsts.APPLICATION_NQUADS;
+        Object result = JsonLdProcessor.toRDF(input, options);
+        assertEquals(TestUtils.join(resultLines, "\n").trim(), ((String) result).trim());
     }
 }
