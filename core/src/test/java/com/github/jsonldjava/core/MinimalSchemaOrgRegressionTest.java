a2e6f8aed492fd6fb50ca920b683067a98c76c94
Pascal Christoph <christoph@hbz-nrw.de>
2020-08-24 17:59:45 +0200

    Remove unused imports
    Complements the last commit.

--- core/src/test/java/com/github/jsonldjava/core/MinimalSchemaOrgRegressionTest.java
+++ core/src/test/java/com/github/jsonldjava/core/MinimalSchemaOrgRegressionTest.java
@@ -12,9 +12,6 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.protocol.RequestAcceptEncoding;
 import org.apache.http.client.protocol.ResponseContentEncoding;
 import org.apache.http.impl.client.CloseableHttpClient;
